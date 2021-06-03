97eee8e494e2f49729aea54a7c2e7865e770049c
Kattusite <Kattusite@users.noreply.github.com>
2021-06-03 18:59:40 -0400

    Add Aikar's optimized flags
    This commit includes the optimized Java JVM flags from mcflags.emc.gs as the defaults, since these are the recommended flags for use with the paper server.

--- serverconf.sh
+++ serverconf.sh
@@ -13,8 +13,19 @@ JAR="paper-$MC_VERSION.jar"
 # JAVA SETTINGS
 JAVA_PKG="openjdk-16-jdk-headless"
 JRE_JAVA="java"
-JVM_ARGS="-Xms4096M -Xmx6144M"
+MIN_RAM="4G"
+MAX_RAM="6144M"
+JVM_ARGS="-Xms$MIN_RAM -Xmx$MAX_RAM"
 JAR_ARGS="-nogui"
+
+# AIKAR'S FLAGS
+# An optimized set of Java / JVM flags designed
+# to make your server as efficient as possible.
+# See https://mcflags.emc.gs/ for more information.
+USE_AIKARS_FLAGS=yes
+if [ "$USE_AIKARS_FLAGS" = "yes" ]; then
+  JVM_ARGS="-Xms$MIN_RAM -Xmx$MAX_RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
+fi
 
 # SERVER FILES
 SERVER_DIR=$PWD             # Path to the server directory
